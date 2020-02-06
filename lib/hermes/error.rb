5e8fbb02e6c0a03095bb887657e3a42aa97dd2d3
Justin Adkins <justin.adkins@mythcoders.com>
2020-02-06 05:10:34 +0000

    Working updates

--- lib/hermes/error.rb
+++ lib/hermes/error.rb
@@ -1,26 +1,16 @@
 # frozen_string_literal: true
 
 module Hermes
-  module Error
-    class HTTPError < StandardError
-      def initialize(status, message, http_method, url)
-        @status = status
-        @error_json = message
-        @http_method = http_method
-        @url = url
-      end
-
-      def message
-        <<-EOS.gsub(/^ {10}/, '')
-          #{@status}: #{@error_json}
-          in response to:
-          #{@http_method.upcase} #{@url}
-        EOS
-      end
-
-      attr_reader :status, :error_json, :http_method, :url
+  class Error < StandardError
+    def initialize(status, message)
+      @status = status
+      @message = message
     end
 
-    DuplicateEntityError = Class.new(StandardError)
+    attr_reader :message, :status
+
+    def to_s
+      "HTTP #{status} - #{message}"
+    end
   end
 end
--- lib/hermes/mail_client.rb
+++ lib/hermes/mail_client.rb
@@ -2,19 +2,21 @@
 
 module Hermes
   class MailClient < BaseClient
-    def new_mail(message)
-      conn = api_connection api_url
-      response = conn.post { |req| req.body = message }
+    class << self
+      def new_mail(message)
+        conn = api_connection api_url
+        response = conn.post { |req| req.body = JSON.dump(message) }
 
-      raise StandardError unless response.success?
+        return true if response.success?
 
-      true
-    end
+        raise Hermes::Error.new(response.status, response.body)
+      end
 
-    private
+      private
 
-    def api_url
-      try_base_url + 'mail'
+      def api_url
+        base_url + 'messages'
+      end
     end
   end
 end
